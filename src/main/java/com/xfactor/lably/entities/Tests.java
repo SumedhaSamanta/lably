b0e7995d0b64b43f3e2aa24e4c677a546e175526
SumedhaSamanta <sumedhasamanta@gmail.com>
2021-05-01 13:10:53 +0530

    commit 1 : added entities

---
+++ src/main/java/com/xfactor/lably/entities/Tests.java
@@ -0,0 +1,25 @@
+package com.xfactor.lably.entities;
+public class Tests
+{
+    private String name;
+    private String description;
+    private double price;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
