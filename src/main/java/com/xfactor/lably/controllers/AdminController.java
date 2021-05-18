ea4ea5f251d37c958e51bc18acfd627e5fed1580
SumedhaSamanta <sumedhasamanta@gmail.com>
2021-05-18 16:33:05 +0530

    small changes

--- src/main/java/com/xfactor/lably/controllers/AdminController.java
+++ src/main/java/com/xfactor/lably/controllers/AdminController.java
@@ -1,45 +1,43 @@
 package com.xfactor.lably.controllers;
 
-import java.util.ArrayList;
+import java.util.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.xfactor.lably.entities.Admin;
+import com.xfactor.lably.repository.AdminRepository;
 
 @RestController
 @RequestMapping("/adminControl")
 public class AdminController
 {
-    ArrayList<Admin> admins = new ArrayList<>();
-
-    @PostMapping("/addAdmin")
+    @Autowired
+    AdminRepository adminRepository;
+    @PostMapping("/registerAdmin")
     public Admin addAdmin(@RequestBody Admin admin) {
-        admins.add(admin);
-        return admin;
+        Admin persistedAdmin = adminRepository.save(admin);
+        return persistedAdmin;
     }
-    @GetMapping("/getAdmins")
-    public ArrayList<Admin> getAdmins()
+    @GetMapping("/getAllAdmins")
+    public List<Admin> getAdmins()
     {
-        for(int i = 1;i<=5;i++)
-        {
-            Admin admin = new Admin();
-            admin.setName("AdminName");
-            admin.setUsername("UserName");
-            admin.setPassword("password");
-            admins.add(admin);
-        }
-        return admins;
+        List<Admin> persistedAdmins = adminRepository.findAll();
+        return persistedAdmins;
     }
-    @GetMapping("/getAdmins/{name}")
-    public Admin getAdmins(@PathVariable String name) {
-        Admin admin = new Admin();
-        for (Admin a1 : admins) {
-            if (a1.getName().equals(name))
+    @GetMapping("/getAdminByUsername")
+    public Admin getAdminByUsername(@RequestParam String username) {
+        Admin admin = null;
+        List<Admin> persistedAdmins = adminRepository.findAll();
+        for (Admin a1 : persistedAdmins) {
+            if (a1.getName().equalsIgnoreCase(username))
             {
                 return a1;
             }
