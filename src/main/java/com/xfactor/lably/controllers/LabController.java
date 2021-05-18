4e911df4a79df8be8a7144a74e75e7937d59063e
SumedhaSamanta <sumedhasamanta@gmail.com>
2021-05-18 22:51:37 +0530

    small changes 2

--- src/main/java/com/xfactor/lably/controllers/LabController.java
+++ src/main/java/com/xfactor/lably/controllers/LabController.java
@@ -1,52 +1,48 @@
 package com.xfactor.lably.controllers;
 
-import java.util.ArrayList;
+import java.util.*;
 
-import com.xfactor.lably.entity.Lab;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/labcontrol")
-public class LabController 
-{
-    ArrayList<Lab> labs = new ArrayList<>();
 
-    @PostMapping("/addLab")
+import com.xfactor.lably.entities.Lab;
+import com.xfactor.lably.repository.LabRepository;
+
+@RestController
+@RequestMapping("/labControl")
+public class LabController
+{
+    @Autowired
+    LabRepository labRepository;
+    @PostMapping("/registerLab")
     public Lab addLab(@RequestBody Lab lab) {
-        labs.add(lab);
-        return lab;
+        Lab persistedLab = labRepository.save(lab);
+        return persistedLab;
     }
-    @GetMapping("/getLabs")
-    public ArrayList<Lab> getLabs()
+    @GetMapping("/getAllLabs")
+    public List<Lab> getLabs()
     {
-        for(int i = 1;i<=5;i++)
-        {
-            Lab lab = new Lab();
-            lab.setAddress("A");
-            lab.setName("LabName");
-            lab.setPhone("phone");
-            lab.setPincode("pincode");
-            labs.add(lab);
-        }
-        return labs;
+        List<Lab> persistedLabs = labRepository.findAll();
+        return persistedLabs;
     }
-    @GetMapping("/getLabs/{name}")
-    public Lab getLabs(@PathVariable String name) {
-        Lab lab = new Lab();
-        for (Lab lab1 : labs) {
-            if (lab1.getName().equals(name))
+    @GetMapping("/getLabByName")
+    public Lab getLabByName(@RequestParam String name) {
+        Lab lab = null;
+        List<Lab> persistedLabs = labRepository.findAll();
+        for (Lab l1 : persistedLabs) {
+            if (l1.getName().equalsIgnoreCase(name))
             {
-                return lab1;
+                return l1;
             }
 
         }
         return lab;
-    }
-
+    }    
 }
