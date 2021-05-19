38c4d6214a499d9356c301f8308517dc4a9ba8e9
SumedhaSamanta <sumedhasamanta@gmail.com>
2021-05-19 17:59:59 +0530

    Custom Queries 1

--- src/main/java/com/xfactor/lably/repository/AdminRepository.java
+++ src/main/java/com/xfactor/lably/repository/AdminRepository.java
@@ -1,7 +1,13 @@
 package com.xfactor.lably.repository;
 import com.xfactor.lably.entities.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
 public interface AdminRepository extends JpaRepository<Admin,Long>
 {
-    
+    Admin findByUsername(String username);
+
+    Admin findByUsernameAndPassword(String username,String password);
+
+    //@Query("SELECT admin FROM Admin admin WHERE admin.username = :username")
+    //List<Admin> retrieveUsername(String username);
 }
